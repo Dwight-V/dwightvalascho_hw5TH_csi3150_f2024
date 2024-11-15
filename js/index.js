c0ebe65b4e06c31bd4b6d9171103a9d63e7fd68a
Dwight-V <dvight@duck.com>
2024-11-15 18:32:14 -0500

    Make filter working

--- js/index.js
+++ js/index.js
@@ -9,15 +9,23 @@ import { usedCars } from './usedCars.js';
 // usedCars[0].price
 // usedCars[0].year
 
+// The div that holds the car cards
 let result = document.querySelector("#result");
+let arrMake = document.querySelectorAll(".car-make");
+let arrColor = document.querySelectorAll(".car-color");
 
 // From https://stackoverflow.com/a/2901298
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
+let capitalize = (str) => {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
 // returns a string containing the html code for displaying a card. 
 let createCard = (car, id) => {
+    // console.log(`createCard() ${car.year}`);
     return `
         <div id="${id}" class="card">
             <h3>${car.year} ${car.make} ${car.model}</h1>
@@ -43,30 +51,74 @@ let createCard = (car, id) => {
     // onClick idea from https://www.w3schools.com/howto/howto_js_read_more.asp
 }
 
+let filterCards = () => {
+    let arrReturn = [];
+    let arrCheckedBoxes = [];
+
+    for (let i = 0; i < arrMake.length; i++) {
+        if (arrMake[i].checked) {
+            arrCheckedBoxes.push(arrMake[i].value);
+        }
+    }
+
+    if (arrCheckedBoxes.length <= 0) {
+        console.log("No checkboxes selected.");
+        return;
+    }
+
+    // .inclueds() from https://stackoverflow.com/a/6116511
+    for (let i = 0; i < usedCars.length; i++) {
+        // console.log(`${usedCars[i].make} in ${arrCheckedBoxes}:    ${arrCheckedBoxes.includes(usedCars[i].make)}`);
+        if (arrCheckedBoxes.includes(usedCars[i].make)) {
+            arrReturn.push(usedCars[i]);
+        }
+    }
+
+    // console.log(arrReturn);
+    return arrReturn;
+}
+
 // Displays cards.
-let displayCards = (colPerRow) => {
+let displayCards = (arrCar, colPerRow = 4) => {
+    if (!arrCar || arrCar.length <= 0) {
+        // console.log("in empty displayCards()");
+        result.innerHTML = `<p class="no-results">No search results.</p>`
+        return;
+    }
+
+
     let strResult = `<div class="result-row">`;
     
     // Starts at 1 since 0 % 4 == 0, making the first element in a row by itself.
-    for (let i = 1; i < usedCars.length; i++) {
-        // console.log(`Beginning  i: ${i}`);
-        strResult += createCard(usedCars[i - 1], `car-${(i - 1)}`);
+    for (let i = 1; i < (arrCar.length + 1); i++) {
+        strResult += createCard(arrCar[i - 1], `car-${(i - 1)}`);
         if (i % colPerRow == 0) {
-            // console.log(`In if  i: ${i}`);
             strResult += `</div><div class="result-row">`;
         }
     }
-    strResult += `</div>`;
 
-    result.innerHTML = strResult;
-
+    result.innerHTML = strResult + `</div>`;
 };
+
+// Displays the entire car array.
+let displayAllCards = () => {
+    displayCards(usedCars);
+}
+
 
 document.querySelector("#btn-search").addEventListener("click", (e) => {
     // prevents page reload on submit
     e.preventDefault();
-
-    console.log(usedCars[0]);
+    // filterCards();
+    displayCards(filterCards());
 });
 
-displayCards(4);
+
+
+
+
+
+
+
+// init
+displayAllCards();
